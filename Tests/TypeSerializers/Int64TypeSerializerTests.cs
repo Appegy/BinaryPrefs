227df52087ddfaa652e8aa5fe648c834c80a72a4
Ivan Murashka <imurashka.me@gmail.com>
2024-06-04 00:15:27 +0200

    Format code

--- Tests/TypeSerializers/Int64TypeSerializerTests.cs
+++ Tests/TypeSerializers/Int64TypeSerializerTests.cs
@@ -8,14 +8,14 @@ namespace Appegy.BinaryStorage.TypeSerializers
     {
         private static long[] Inputs => new[]
         {
-            long.MinValue,  // -9223372036854775808
-            long.MaxValue,  // 9223372036854775807
-            0L,             // zero
-            1L,             // smallest positive long
-            -1L,            // smallest negative long
-            1024L,          // power of two
-            -1024L,         // negative power of two
-            1234567890123L  // random positive value
+            long.MinValue, // -9223372036854775808
+            long.MaxValue, // 9223372036854775807
+            0L, // zero
+            1L, // smallest positive long
+            -1L, // smallest negative long
+            1024L, // power of two
+            -1024L, // negative power of two
+            1234567890123L // random positive value
         };
 
         public Int64TypeSerializerTests(long value) : base(value)
