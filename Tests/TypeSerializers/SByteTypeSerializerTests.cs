452cdfd73ea81b93288b6f610c247728802a0cc7
Ivan Murashka <imurashka.me@gmail.com>
2024-06-03 23:54:01 +0200

    SByteSerializer

---
+++ Tests/TypeSerializers/SByteTypeSerializerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Appegy.BinaryStorage.TypeSerializers
+{
+    [TestFixture]
+    [TestFixtureSource(nameof(Inputs))]
+    internal class SByteTypeSerializerTests : BaseTypeSerializerTests<sbyte, SByteSerializer>
+    {
+        private static sbyte[] Inputs => new[]
+        {
+            sbyte.MinValue, // -128
+            sbyte.MaxValue, // 127
+            (sbyte)0, // zero
+            (sbyte)1, // smallest positive sbyte
+            (sbyte)-1, // smallest negative sbyte
+            (sbyte)64, // positive power of two
+            (sbyte)-64, // negative power of two
+            (sbyte)10, // random positive value
+            (sbyte)-10 // random negative value
+        };
+
+        public SByteTypeSerializerTests(sbyte value) : base(value)
+        {
+        }
+    }
+}
