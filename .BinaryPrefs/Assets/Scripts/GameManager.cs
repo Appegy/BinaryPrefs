e219451e7112cb1d8b6a0cbff8030faddd89ee94
Ivan Murashka <imurashka.me@gmail.com>
2024-06-03 19:38:15 +0200

    prepare package and sample structure

---
+++ .BinaryPrefs/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Appegy.UniLogger.Example
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("Game started");
+        }
+    }
+}
