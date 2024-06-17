523506e1ef8f3b5f0656cb673651170be51fcdf2
Ivan Murashka <imurashka.me@gmail.com>
2024-06-17 23:50:38 +0200

    add KeyValueTypeSerializer with tests

---
+++ Runtime/Serialization/KeyValueTypeSerializer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Appegy.BinaryStorage
+{
+    public class KeyValueTypeSerializer<TKey, TValue> : TypeSerializer<KeyValuePair<TKey, TValue>>
+    {
+        private readonly TypeSerializer<TKey> _keySerializer;
+        private readonly TypeSerializer<TValue> _valueSerializer;
+
+        public override string TypeName { get; }
+
+        public KeyValueTypeSerializer(TypeSerializer<TKey> t1, TypeSerializer<TValue> t2)
+        {
+            _keySerializer = t1;
+            _valueSerializer = t2;
+            TypeName = $"{typeof(TKey).Name}:{typeof(TValue).Name}";
+        }
+
+        public override int SizeOf(KeyValuePair<TKey, TValue> value)
+        {
+            return _keySerializer.SizeOf(value.Key) + _valueSerializer.SizeOf(value.Value);
+        }
+
+        public override bool Equals(KeyValuePair<TKey, TValue> value1, KeyValuePair<TKey, TValue> value2)
+        {
+            return _keySerializer.Equals(value1.Key, value2.Key) && _valueSerializer.Equals(value1.Value, value2.Value);
+        }
+
+        public override void WriteTo(BinaryWriter writer, KeyValuePair<TKey, TValue> value)
+        {
+            _keySerializer.WriteTo(writer, value.Key);
+            _valueSerializer.WriteTo(writer, value.Value);
+        }
+
+        public override KeyValuePair<TKey, TValue> ReadFrom(BinaryReader reader)
+        {
+            return new KeyValuePair<TKey, TValue>(_keySerializer.ReadFrom(reader), _valueSerializer.ReadFrom(reader));
+        }
+    }
+}
---
+++ Tests/TypeSerializers/KeyValueTypeSerializerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Appegy.BinaryStorage.TypeSerializers
+{
+    [TestFixture]
+    [TestFixtureSource(nameof(Inputs))]
+    public class KeyValueTypeSerializerTests : TypeSerializerTests<KeyValuePair<string, int>, KeyValueTypeSerializer<string, int>>
+    {
+        public KeyValueTypeSerializerTests(KeyValuePair<string, int> defaultValue)
+            : base(defaultValue, new KeyValueTypeSerializer<string, int>(StringSerializer.Shared, Int32Serializer.Shared))
+        {
+        }
+
+        private static KeyValuePair<string, int>[] Inputs => new[]
+        {
+            new KeyValuePair<string, int>("key1", 1),
+            new KeyValuePair<string, int>("key2", 2),
+            new KeyValuePair<string, int>("key3", 3),
+            new KeyValuePair<string, int>("", 0), // empty key
+            new KeyValuePair<string, int>(null, 0), // null key
+            new KeyValuePair<string, int>("key with space", 12345),
+            new KeyValuePair<string, int>("long key with multiple words", 67890),
+        };
+    }
+}
