d02e2e214526f6eb6abb4f88819716e0dc3d67fb
Ivan Murashka <imurashka.me@gmail.com>
2024-08-29 21:59:46 +0200

    float and string tests

--- Tests/BinaryPrefsTests.cs
+++ Tests/BinaryPrefsTests.cs
@@ -8,7 +8,7 @@ namespace Appegy.Tests.Storage
     [TestFixture]
     public class BinaryPrefsIntTests
     {
-        [SetUp]
+        [SetUp, TearDown]
         public void SetUp()
         {
             // Clear PlayerPrefs and BinaryPrefs before each test
@@ -16,11 +16,13 @@ namespace Appegy.Tests.Storage
             BinaryPrefs.DeleteAll();
         }
 
+        #region Integer
+
         [Test]
         public void SetInt_ShouldStoreValueInBinaryStorage()
         {
             // Arrange
-            var key = "testInt";
+            var key = "key";
             var value = 42;
 
             // Act
@@ -34,7 +36,7 @@ namespace Appegy.Tests.Storage
         public void GetInt_ShouldReturnDefaultValueIfKeyNotFound()
         {
             // Arrange
-            var key = "unknownInt";
+            var key = "unknownKey";
             var defaultValue = 10;
 
             // Act
@@ -48,7 +50,7 @@ namespace Appegy.Tests.Storage
         public void GetInt_ShouldReturnValueFromPlayerPrefsIfNotInBinaryStorage()
         {
             // Arrange
-            var key = "testInt";
+            var key = "key";
             var playerPrefsValue = 100;
 
             // Store value in PlayerPrefs only
@@ -82,7 +84,7 @@ namespace Appegy.Tests.Storage
         public void SetInt_ShouldOverrideExistingValueInBinaryStorage()
         {
             // Arrange
-            var key = "testInt";
+            var key = "key";
             var initialValue = 42;
             var newValue = 84;
 
@@ -95,5 +97,175 @@ namespace Appegy.Tests.Storage
             // Assert
             BinaryPrefs.GetInt(key).Should().Be(newValue);
         }
+
+        #endregion
+
+        #region Float
+
+        [Test]
+        public void SetFloat_ShouldStoreValueInBinaryStorage()
+        {
+            // Arrange
+            var key = "key";
+            var value = 42f;
+
+            // Act
+            BinaryPrefs.SetFloat(key, value);
+
+            // Assert
+            BinaryPrefs.GetFloat(key).Should().Be(value);
+        }
+
+        [Test]
+        public void GetFloat_ShouldReturnDefaultValueIfKeyNotFound()
+        {
+            // Arrange
+            var key = "unknownKey";
+            var defaultValue = 10f;
+
+            // Act
+            var result = BinaryPrefs.GetFloat(key, defaultValue);
+
+            // Assert
+            result.Should().Be(defaultValue);
+        }
+
+        [Test]
+        public void GetFloat_ShouldReturnValueFromPlayerPrefsIfNotInBinaryStorage()
+        {
+            // Arrange
+            var key = "key";
+            var playerPrefsValue = 100f;
+
+            // Store value in PlayerPrefs only
+            PlayerPrefs.SetFloat(key, playerPrefsValue);
+
+            // Act
+            var result = BinaryPrefs.GetFloat(key);
+
+            // Assert
+            result.Should().Be(playerPrefsValue);
+
+            // Verify that the value is now stored in BinaryStorage
+            BinaryPrefs.GetFloat(key).Should().Be(playerPrefsValue);
+        }
+
+        [Test]
+        public void GetFloat_ShouldReturnDefaultValueIfKeyNotFoundInBothStorages()
+        {
+            // Arrange
+            var key = "nonExistentKey";
+            var defaultValue = 20f;
+
+            // Act
+            var result = BinaryPrefs.GetFloat(key, defaultValue);
+
+            // Assert
+            result.Should().Be(defaultValue);
+        }
+
+        [Test]
+        public void SetFloat_ShouldOverrideExistingValueInBinaryStorage()
+        {
+            // Arrange
+            var key = "key";
+            var initialValue = 42f;
+            var newValue = 84f;
+
+            // Store initial value
+            BinaryPrefs.SetFloat(key, initialValue);
+
+            // Act
+            BinaryPrefs.SetFloat(key, newValue);
+
+            // Assert
+            BinaryPrefs.GetFloat(key).Should().Be(newValue);
+        }
+
+        #endregion
+
+        #region String
+
+        [Test]
+        public void SetString_ShouldStoreValueInBinaryStorage()
+        {
+            // Arrange
+            var key = "key";
+            var value = "42";
+
+            // Act
+            BinaryPrefs.SetString(key, value);
+
+            // Assert
+            BinaryPrefs.GetString(key).Should().Be(value);
+        }
+
+        [Test]
+        public void GetString_ShouldReturnDefaultValueIfKeyNotFound()
+        {
+            // Arrange
+            var key = "unknownKey";
+            var defaultValue = "10";
+
+            // Act
+            var result = BinaryPrefs.GetString(key, defaultValue);
+
+            // Assert
+            result.Should().Be(defaultValue);
+        }
+
+        [Test]
+        public void GetString_ShouldReturnValueFromPlayerPrefsIfNotInBinaryStorage()
+        {
+            // Arrange
+            var key = "key";
+            var playerPrefsValue = "100";
+
+            // Store value in PlayerPrefs only
+            PlayerPrefs.SetString(key, playerPrefsValue);
+
+            // Act
+            var result = BinaryPrefs.GetString(key);
+
+            // Assert
+            result.Should().Be(playerPrefsValue);
+
+            // Verify that the value is now stored in BinaryStorage
+            BinaryPrefs.GetString(key).Should().Be(playerPrefsValue);
+        }
+
+        [Test]
+        public void GetString_ShouldReturnDefaultValueIfKeyNotFoundInBothStorages()
+        {
+            // Arrange
+            var key = "nonExistentKey";
+            var defaultValue = "20";
+
+            // Act
+            var result = BinaryPrefs.GetString(key, defaultValue);
+
+            // Assert
+            result.Should().Be(defaultValue);
+        }
+
+        [Test]
+        public void SetString_ShouldOverrideExistingValueInBinaryStorage()
+        {
+            // Arrange
+            var key = "key";
+            var initialValue = "42";
+            var newValue = "84";
+
+            // Store initial value
+            BinaryPrefs.SetString(key, initialValue);
+
+            // Act
+            BinaryPrefs.SetString(key, newValue);
+
+            // Assert
+            BinaryPrefs.GetString(key).Should().Be(newValue);
+        }
+
+        #endregion
     }
 }
