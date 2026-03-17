a9659586a0660675d23d4f57088453e7185a01f3
Ivan Murashka <imurashka.me@gmail.com>
2026-03-17 13:19:25 +0100

    Bump version to 1.0.5

--- Runtime/PackageInfo.cs
+++ Runtime/PackageInfo.cs
@@ -9,7 +9,7 @@ namespace Appegy.Storage
     public static class PackageInfo
     {
         public const string Name = "com.appegy.binary-prefs";
-        public const string Version = "1.0.4";
+        public const string Version = "1.0.5";
         public static string PersistentFolder => Path.Combine(Application.persistentDataPath, Name);
     }
 }
