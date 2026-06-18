ad42b1d5825f44268dbcce7225adb243538149ed
Ivan Murashka <imurashka.me@gmail.com>
2026-06-19 00:47:13 +0200

    style: collapse fallback section lookup to one line

--- src/Runtime/BinaryStorageIO.cs
+++ src/Runtime/BinaryStorageIO.cs
@@ -132,8 +132,7 @@ namespace Appegy.Storage
                     // #04 <---> Read name of type in serializer
                     var serializerName = reader.ReadString();
                     sectionsNames[i] = serializerName;
-                    orderedSectionsFromFile[i] = sections.FirstOrDefault(c => c.TypeName == serializerName)
-                        ?? sections.FirstOrDefault(c => c.FallbackNames.Contains(serializerName));
+                    orderedSectionsFromFile[i] = sections.FirstOrDefault(c => c.TypeName == serializerName) ?? sections.FirstOrDefault(c => c.FallbackNames.Contains(serializerName));
                 }
 
                 // #05 <---> Read amount of records in storage
